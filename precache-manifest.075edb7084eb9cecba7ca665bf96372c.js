39da1ac5a58ee7396efdcc61cd55d4239a2565c9
Erickson Guffey <eguffeyarmy2@gmail.com>
2020-03-04 12:15:31 -0500

    Updates

---
+++ precache-manifest.075edb7084eb9cecba7ca665bf96372c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b75a9a3b9dbb0166274ef68ef02f15e3",
+    "url": "/KahootClone/index.html"
+  },
+  {
+    "revision": "de64c781d17ef5983645",
+    "url": "/KahootClone/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6f9e78794ae9716085f4",
+    "url": "/KahootClone/static/js/2.7cf76a36.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/KahootClone/static/js/2.7cf76a36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de64c781d17ef5983645",
+    "url": "/KahootClone/static/js/main.085be666.chunk.js"
+  },
+  {
+    "revision": "ef960f497b5b4bb2e315",
+    "url": "/KahootClone/static/js/runtime-main.589250d4.js"
+  }
+]);
